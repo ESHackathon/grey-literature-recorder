848e5395a6c661fe6e79babe0ff376b016e14f10
Christopher Penkin <chris@digitalsolutionfoundry.co.za>
2019-08-25 23:37:23 +0200

    Added ability to download the results.

--- src/js/content.js
+++ src/js/content.js
@@ -1,5 +1,167 @@
 'use strict'
 
+/*
+* FileSaver.js
+* A saveAs() FileSaver implementation.
+*
+* By Eli Grey, http://eligrey.com
+*
+* License : https://github.com/eligrey/FileSaver.js/blob/master/LICENSE.md (MIT)
+* source  : http://purl.eligrey.com/github/FileSaver.js
+*/
+
+// The one and only way of getting global scope in all environments
+// https://stackoverflow.com/q/3277182/1008999
+var _global = typeof window === 'object' && window.window === window
+    ? window : typeof self === 'object' && self.self === self
+        ? self : typeof global === 'object' && global.global === global
+            ? global
+            : this
+
+function bom (blob, opts) {
+    if (typeof opts === 'undefined') opts = { autoBom: false }
+    else if (typeof opts !== 'object') {
+        opts = { autoBom: !opts }
+    }
+
+    // prepend BOM for UTF-8 XML and text/* types (including HTML)
+    // note: your browser will automatically convert UTF-16 U+FEFF to EF BB BF
+    if (opts.autoBom && /^\s*(?:text\/\S*|application\/xml|\S*\/\S*\+xml)\s*;.*charset\s*=\s*utf-8/i.test(blob.type)) {
+        return new Blob([String.fromCharCode(0xFEFF), blob], { type: blob.type })
+    }
+    return blob
+}
+
+function download (url, name, opts) {
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', url)
+    xhr.responseType = 'blob'
+    xhr.onload = function () {
+        saveAs(xhr.response, name, opts)
+    }
+    xhr.onerror = function () {
+        console.error('could not download file')
+    }
+    xhr.send()
+}
+
+function corsEnabled (url) {
+    var xhr = new XMLHttpRequest()
+    // use sync to avoid popup blocker
+    xhr.open('HEAD', url, false)
+    try {
+        xhr.send()
+    } catch (e) {}
+    return xhr.status >= 200 && xhr.status <= 299
+}
+
+// `a.click()` doesn't work for all browsers (#465)
+function click (node) {
+    try {
+        node.dispatchEvent(new MouseEvent('click'))
+    } catch (e) {
+        var evt = document.createEvent('MouseEvents')
+        evt.initMouseEvent('click', true, true, window, 0, 0, 0, 80,
+            20, false, false, false, false, 0, null)
+        node.dispatchEvent(evt)
+    }
+}
+
+var saveAs = _global.saveAs || (
+    // probably in some web worker
+    (typeof window !== 'object' || window !== _global)
+        ? function saveAs () { /* noop */ }
+
+        // Use download attribute first if possible (#193 Lumia mobile)
+        : 'download' in HTMLAnchorElement.prototype
+        ? function saveAs (blob, name, opts) {
+            var URL = _global.URL || _global.webkitURL
+            var a = document.createElement('a')
+            name = name || blob.name || 'download'
+
+            a.download = name
+            a.rel = 'noopener' // tabnabbing
+
+            // TODO: detect chrome extensions & packaged apps
+            // a.target = '_blank'
+
+            if (typeof blob === 'string') {
+                // Support regular links
+                a.href = blob
+                if (a.origin !== location.origin) {
+                    corsEnabled(a.href)
+                        ? download(blob, name, opts)
+                        : click(a, a.target = '_blank')
+                } else {
+                    click(a)
+                }
+            } else {
+                // Support blobs
+                a.href = URL.createObjectURL(blob)
+                setTimeout(function () { URL.revokeObjectURL(a.href) }, 4E4) // 40s
+                setTimeout(function () { click(a) }, 0)
+            }
+        }
+
+        // Use msSaveOrOpenBlob as a second approach
+        : 'msSaveOrOpenBlob' in navigator
+            ? function saveAs (blob, name, opts) {
+                name = name || blob.name || 'download'
+
+                if (typeof blob === 'string') {
+                    if (corsEnabled(blob)) {
+                        download(blob, name, opts)
+                    } else {
+                        var a = document.createElement('a')
+                        a.href = blob
+                        a.target = '_blank'
+                        setTimeout(function () { click(a) })
+                    }
+                } else {
+                    navigator.msSaveOrOpenBlob(bom(blob, opts), name)
+                }
+            }
+
+            // Fallback to using FileReader and a popup
+            : function saveAs (blob, name, opts, popup) {
+                // Open a popup immediately do go around popup blocker
+                // Mostly only available on user interaction and the fileReader is async so...
+                popup = popup || open('', '_blank')
+                if (popup) {
+                    popup.document.title =
+                        popup.document.body.innerText = 'downloading...'
+                }
+
+                if (typeof blob === 'string') return download(blob, name, opts)
+
+                var force = blob.type === 'application/octet-stream'
+                var isSafari = /constructor/i.test(_global.HTMLElement) || _global.safari
+                var isChromeIOS = /CriOS\/[\d]+/.test(navigator.userAgent)
+
+                if ((isChromeIOS || (force && isSafari)) && typeof FileReader === 'object') {
+                    // Safari doesn't allow downloading of blob URLs
+                    var reader = new FileReader()
+                    reader.onloadend = function () {
+                        var url = reader.result
+                        url = isChromeIOS ? url : url.replace(/^data:[^;]*;/, 'data:attachment/file;')
+                        if (popup) popup.location.href = url
+                        else location = url
+                        popup = null // reverse-tabnabbing #460
+                    }
+                    reader.readAsDataURL(blob)
+                } else {
+                    var URL = _global.URL || _global.webkitURL
+                    var url = URL.createObjectURL(blob)
+                    if (popup) popup.location = url
+                    else location.href = url
+                    popup = null // reverse-tabnabbing #460
+                    setTimeout(function () { URL.revokeObjectURL(url) }, 4E4) // 40s
+                }
+            }
+)
+
+
+
 const STEP_SEARCH_INPUT = 1;
 const STEP_SELECT_ELEMENT = 2;
 const STEP_ANNOTATE = 3;
@@ -426,9 +588,7 @@ function getData() {
         localStorage.setItem('nextUrl', nextUrl);
         window.location.href = nextUrl;
     } else {
-        chrome.runtime.sendMessage({
-            type: 'STEP_NEXT'
-        });
+        createFiles()
     }
 }
 
@@ -479,15 +639,17 @@ function checkContinue() {
 }
 
 
-function createFiles(data) {
+function createFiles() {
+    // Create CSV File
     let csvData = '';
 
     annotations.forEach(function (annotation) {
-        csvData += '"' + annotation['title'] + '",'
+        csvData += '"' + annotation['title'] + '"\t'
     });
 
     csvData += '\r\n';
 
+    const data = JSON.parse(localStorage.getItem('data'))
     data.data.forEach(function (data) {
         annotations.forEach(function (annotation) {
             let item = data[annotation['title']]
@@ -498,14 +660,32 @@ function createFiles(data) {
                 item = item.replace(/\r?\n|\r/g, ' ')
             }
 
-            csvData += '"' + item + '",'
+            csvData += '"' + item + '"\t'
         });
         csvData += '\r\n';
     });
 
     const utc = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-    const blob = new Blob([csvData], {type: 'text/plain;charset=utf-8'});
-    saveAs(blob, 'grey_lit_recorder_session_' + utc + '.csv');
+    const csvBlob = new Blob([csvData], {type: 'text/plain;charset=utf-8'});
+    saveAs(csvBlob, 'grey_lit_recorder_session_' + utc + '.csv');
+
+    // Create Text File
+    let text = '';
+    text += 'Grey Lit Recorder Session\r\n';
+    text += '=========================\r\n';
+    text += 'Date Run: ' + data.date + '\r\n';
+    text += 'Website URL: ' + data.url + '\r\n';
+    text += 'Search Term: ' + data.searchTerm + '\r\n';
+    text += 'Results Returned: ' + data.data.length + '\r\n';
+    text += '\r\n';
+    text += 'Environment:\r\n';
+    text += '-----------\r\n';
+    text += 'Operating System: ' + data.environment.osName + '\r\n';
+    text += 'Web Browser: ' + data.environment.browserName + '\r\n';
+    text += 'Web Browser Version: ' + data.environment.fullVersion + '\r\n';
+
+    const textBlob = new Blob([text], {type: 'text/plain;charset=utf-8'});
+    saveAs(textBlob, 'grey_lit_recorder_session_' + utc + '.txt');
 
     hideVeil();
     hideUI();
